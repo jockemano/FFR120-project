ffc8f3fe773a9868a22f9bc4c6511b8fa937f43b
ruxin-z <74582790+ruxin-z@users.noreply.github.com>
2020-11-26 00:42:22 +0800

    Update main.m

--- main.m
+++ main.m
@@ -2,15 +2,11 @@ clear all
 clc
 % addpath('classes/bug-model')
 % addpath('classes/house-model')
-AREA_SIZE = 10;
+AREA_SIZE = 50;
 
 numberOfBugs = 1;
 time = 1;
-numberOfTime = 100;
-numberOfSteps = 30;
-numberOfRandomSteps = 30;
-currentPositionX = zeros(numberOfSteps,1);
-currentPositionY = zeros(numberOfSteps,1);
+TIME_LIMIT = 40;
 
 initialPosition = randi(AREA_SIZE,1,2);
 X = [];
@@ -19,6 +15,8 @@ Y = [];
 bug(1) = Bug(initialPosition(1), initialPosition(1), AREA_SIZE);
 X = [X;bug(1).row];
 Y = [Y;bug(1).col];
+
+numberOfLivingBugs = zeros(TIME_LIMIT,1);
 
 figure(1)
 hold on
@@ -31,30 +29,32 @@ xlabel('X')
 ylabel('Y')
 
 
-while (time <= numberOfTime)
+while (time <= TIME_LIMIT)
 
     validStandard = 1;    
     numberOfBugs = numel(bug);
+
     for nBug = 1:numberOfBugs
-        validStandard = input('Whether the place is valid or not? ');
+        numberOfBugs = numel(bug);
+        %validStandard = input('Whether the place is valid or not? ');
         bug(nBug) = bug(nBug).move(validStandard);
         X(nBug) = bug(nBug).row;
         Y(nBug) = bug(nBug).col;
         bug(nBug) = bug(nBug).grow();
         if (bug(nBug).age == 3)
-            bug(numberOfBugs+1) = Bug(bug(numberOfBugs).row,bug(numberOfBugs).col,AREA_SIZE);
-            X = [X;bug(numberOfBugs+1).row];
-            Y = [Y;bug(numberOfBugs+1).col];
+            bug(nBug) = bug(nBug).reproduce;
+            for iEgg = 1:bug(nBug).egg
+                bug(numberOfBugs+iEgg) = Bug(bug(numberOfBugs).row,bug(numberOfBugs).col,AREA_SIZE);
+                X = [X;bug(numberOfBugs+iEgg).row];
+                Y = [Y;bug(numberOfBugs+iEgg).col];
+            end
         end
         
         if (bug(nBug).age >= 10)
             bug(nBug) = bug(nBug).die;
         end
-    end
-    
-    X = mod(X,AREA_SIZE);
-    Y = mod(Y,AREA_SIZE);    
-    
+    end   
+         
     % Plot
     if rem(time,1)==0
         cla
@@ -64,5 +64,8 @@ while (time <= numberOfTime)
         drawnow update
     end
     
+    numberOfLivingBugs(time) = length(find(~isnan(X)));
+    
     time = time + 1;
+    
 end
